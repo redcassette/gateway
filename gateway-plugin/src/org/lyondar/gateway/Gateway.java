d2391a5e1dfad01c267504059df972f7c5c2d55d
ulincsys <ulincsys@Ulincsys-Ryzen>
2020-04-09 15:37:56 -0500

    Initial commit - Blank plugin with pre-configured Spigot API jar

---
+++ gateway-plugin/src/org/lyondar/gateway/Gateway.java
@@ -0,0 +1,14 @@
+package org.lyondar.gateway;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Gateway extends JavaPlugin{
+	// Fired when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+}
